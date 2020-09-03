3cff6fddac7914eb4c620cc6aedc7fa066a58fc6
Jem Pool <jem.suarez@correounivalle.edu.co>
2020-09-03 16:59:35 -0500

    working

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Paperbase from './template/Paperbase';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Paperbase />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
