e44b934fc97930dfca9621ea21d44092d8232c9f
Jem Pool <jem.suarez@correounivalle.edu.co>
2020-09-06 11:42:22 -0500

    y

--- travis_merge_script.sh
+++ travis_merge_script.sh
@@ -1,10 +1,12 @@
-if [ "$TRAVIS_BRANCH" != "dev" ]; then 
-    exit 0;
+if [ "$TRAVIS_BRANCH" != "dev" ]; 
+    then
+        exit 0;
+    else
+        export GIT_COMMITTER_EMAIL="jem.suarez@correounivalle.edu.co"
+        export GIT_COMMITTER_NAME="jempool"
+        git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
+        git fetch --all || exit
+        git checkout master || exit
+        git merge --no-ff "$TRAVIS_COMMIT" || exit
+        git push @github.com/">https://${GITHUB_TOKEN}@github.com/jempool/paperbase.git        
 fi
-export GIT_COMMITTER_EMAIL="jem.suarez@correounivalle.edu.co"
-export GIT_COMMITTER_NAME="jempool"
-git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
-git fetch --all || exit
-git checkout master || exit
-git merge --no-ff "$TRAVIS_COMMIT" || exit
-git push @github.com/">https://${GITHUB_TOKEN}@github.com/jempool/paperbase.git
