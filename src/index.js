469f79b0c1aa999a4084a767cc4c9d7f15295e13
Jem Pool <jem.suarez@correounivalle.edu.co>
2020-09-26 07:55:49 -0500

    PWA test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Paperbase from './dashboard/Paperbase';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Paperbase from "./dashboard/Paperbase";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <Paperbase />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
